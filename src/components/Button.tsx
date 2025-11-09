1fa7c355503013bc90ef74dc3c56f3c807752d31
Felipe Leite <lipi4shared@hotmail.com>
2025-11-08 23:25:59 -0300

    Melhorando botão default.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,19 @@ type ButtonProps = ComponentProps<'button'> & {
 }
 
 export function Button({ className, variant = 'default', ...props }: ButtonProps) {
+	if(variant === 'default') {
+		return (
+			<button 
+				className={twMerge('relative group overflow-hidden bg-transparent border-0 text-white font-semibold text-base py-3 px-8 rounded-full shadow-lg cursor-pointer', className)}
+				{...props}
+			>
+				<span className="absolute inset-0 bg-linear-to-r from-purple-500 to-pink-500 transition-all duration-300 ease-in-out group-hover:from-purple-600 group-hover:to-pink-600"></span>
+				<span className="absolute inset-0.5 bg-gray-900 group-hover:bg-black/10 rounded-full transition-colors duration-300 ease-in-out"></span>
+				<span className="relative z-10">ENTRAR EM CONTATO</span>
+			</button>
+		)
+	}
+
 	let variantClasses = twMerge(
 		'flex justify-center items-center gap-2 px-6 py-2 font-medium cursor-pointer hover:opacity-90 transition-all duration-300 w-fit uppercase relative overflow-hidden',
 		'after:content-[""] after:absolute after:top-[-50%] after:left-[-25%] after:w-[150%] after:h-[200%] after:bg-[linear-gradient(45deg,transparent,rgba(255,255,255,0.3),transparent)] after:translate-x-[-100%] after:transition-all after:duration-[0.6s] hover:after:translate-x-[100%]'
@@ -14,14 +27,6 @@ export function Button({ className, variant = 'default', ...props }: ButtonProps
 	switch(variant) {
 		case 'white':
 			variantClasses += twMerge(variantClasses, 'bg-white text-gray-800 rounded-md')
-			break
-
-		case 'default':
-			variantClasses += twMerge(
-				variantClasses, 
-				'rounded-xl text-white bg-linear-to-r from-blue-900 to-pink-800 shadow-lg shadow-pink-500/20 md:hover:scale-105',
-				'border border-[#5e3268]'
-			)
 			break
 	}
 
